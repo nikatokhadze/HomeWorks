1aed9ebd58f13e36ff466b22c95df08adc1bd7d0
ToKHa <103684894+nikatokhadze@users.noreply.github.com>
2022-05-04 21:25:47 +0400

    homework4

---
+++ homework4/homework4/dictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace homework4
+{
+    internal class dictionary<T1, T2>
+    {
+        public dictionary()
+        {
+        }
+
+        internal bool containskey(int f)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
