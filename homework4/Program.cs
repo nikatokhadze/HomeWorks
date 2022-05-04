0c1e2af79c8acadcd8781738f520d71ba5856a42
ToKHa <103684894+nikatokhadze@users.noreply.github.com>
2022-05-04 22:23:28 +0400

    week6

--- homework4/Program.cs
+++ homework4/Program.cs
@@ -13,84 +13,84 @@ namespace homework4
         {
             #region task 1
 
-            //int array;
+            int array;
 
-            //Console.WriteLine("enteri array size: ");
-            //array = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enteri array size: ");
+            array = Convert.ToInt32(Console.ReadLine());
 
-            //Console.WriteLine("write number");
-            //int[] arr = new int[array];
+            Console.WriteLine("write number");
+            int[] arr = new int[array];
 
-            //for (int i = 0; i < arr.Length; i++)
-            //{
-            //    arr[i] = Convert.ToInt32(Console.ReadLine());
-            //}
-            //Console.Write("even: ");
-            //for (int i = 0; i < array; i++)
-            //{
-            //    if (arr[i] % 2 == 0)
-            //    {
-            //        Console.Write(arr[i] + " ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Write("even: ");
+            for (int i = 0; i < array; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    Console.Write(arr[i] + " ");
 
-            //    }
-            //}
-            //Console.WriteLine();
-            //Console.Write("odd: ");
-            //for (int i = 0; i < array; i++)
-            //{
-            //    if (arr[i] % 2! == 1)
-            //    {
-            //        Console.Write(arr[i] + " ");
-            //    }
-            //}
+                }
+            }
+            Console.WriteLine();
+            Console.Write("odd: ");
+            for (int i = 0; i < array; i++)
+            {
+                if (arr[i] % 2! == 1)
+                {
+                    Console.Write(arr[i] + " ");
+                }
+            }
             #endregion
 
             #region task 2
-            //int num;
+            int num;
 
-            //Console.WriteLine("enteri array size: ");
-            //num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enteri array size: ");
+            num = Convert.ToInt32(Console.ReadLine());
 
-            //Console.WriteLine("write number");
-            //int[] arr1 = new int[num];
+            Console.WriteLine("write number");
+            int[] arr1 = new int[num];
 
-            //for (int i = 0; i < arr1.Length; i++)
-            //{
-            //    arr1[i] = Convert.ToInt32(Console.ReadLine());
-            //}
-            //var duplicates = arr1.GroupBy(a => a)
-            //    .Where(b => b.Count() > 1)
-            //    .Select(c => new { item = c.Key, count = c.Count(), sum = c.Sum() })
-            //    .ToList();
-            //Console.WriteLine(String.Join("\n", duplicates));
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                arr1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            var duplicates = arr1.GroupBy(a => a)
+                .Where(b => b.Count() > 1)
+                .Select(c => new { item = c.Key, count = c.Count(), sum = c.Sum() })
+                .ToList();
+            Console.WriteLine(String.Join("\n", duplicates));
 
             #endregion
 
             #region task 3
 
-            //var babys = new List<int>();
-            //babys.Add(1);
-            //babys.Add(2);
-            //babys.Add(3);
-            //babys.Add(4);
-            //babys.Add(5);
-            //babys.Add(6);
-            //babys.Add(7);
-            //babys.Add(8);
-            //babys.Add(9);
-            //babys.Add(10);
+            var babys = new List<int>();
+            babys.Add(1);
+            babys.Add(2);
+            babys.Add(3);
+            babys.Add(4);
+            babys.Add(5);
+            babys.Add(6);
+            babys.Add(7);
+            babys.Add(8);
+            babys.Add(9);
+            babys.Add(10);
 
-            //Console.Write("top must be displayed : ");
-            //int m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("top must be displayed : ");
+            int m = Convert.ToInt32(Console.ReadLine());
 
-            //babys.Reverse();
+            babys.Reverse();
 
-            //Console.Write($"The top {m} are: \n");
-            //foreach (int candy in babys.Take(m))
-            //{
-            //    Console.WriteLine(candy);
-            //}
-            //Console.ReadLine();
+            Console.Write($"The top {m} are: \n");
+            foreach (int candy in babys.Take(m))
+            {
+                Console.WriteLine(candy);
+            }
+            Console.ReadLine();
 
             #endregion
         }
