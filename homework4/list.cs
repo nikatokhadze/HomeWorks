88fa664f75e43a2d8bdfbea83f2c7ddc92f9cbee
ToKHa <103684894+nikatokhadze@users.noreply.github.com>
2022-05-04 21:30:26 +0400

    week6 homework

---
+++ homework4/list.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace homework4
+{
+    internal class list<T>
+    {
+        public list()
+        {
+        }
+
+        internal void Sort()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void reverse()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal IEnumerable<int> take(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal IEnumerable<int> Take(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+      
+    }
+}
