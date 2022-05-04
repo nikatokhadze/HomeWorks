e1704d0b07d18f0feb727937a0aefcf77309b56f
ToKHa <103684894+nikatokhadze@users.noreply.github.com>
2022-05-04 21:41:19 +0400

    changed

--- homework4/homework4/Program.cs
+++ homework4/homework4/Program.cs
@@ -14,56 +14,56 @@ namespace homework4
 
             #region task 1
 
-            //int array;
+            int array;
 
-            //Console.WriteLine("enteri array size: ");
-            //array = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enteri array size: ");
+            array = Convert.ToInt32(Console.ReadLine());
 
-            //Console.WriteLine("write number");
-            //int[] arr = new int[array];
+            Console.WriteLine("write number");
+            int[] arr = new int[array];
 
-            //for (int i = 0; i < arr.Length; i++)
-            //{
-            //    arr[i] = Convert.ToInt32(Console.ReadLine());
-            //}
-            //Console.Write("even: ");
-            //for (int i = 0; i < array; i++)
-            //{
-            //    if (arr[i] % 2 == 0)
-            //    {
-            //        Console.Write(arr[i] + " ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Write("even: ");
+            for (int i = 0; i < array; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    Console.Write(arr[i] + " ");
 
-            //    }
-            //}
-            //Console.WriteLine();
-            //Console.Write("odd: ");
-            //for (int i = 0; i < array; i++)
-            //{
-            //    if (arr[i] % 2! == 1)
-            //    {
-            //        Console.Write(arr[i] + " ");
-            //    }
-            //}
+                }
+            }
+            Console.WriteLine();
+            Console.Write("odd: ");
+            for (int i = 0; i < array; i++)
+            {
+                if (arr[i] % 2! == 1)
+                {
+                    Console.Write(arr[i] + " ");
+                }
+            }
             #endregion
 
             #region task 2
-            //int num;
+            int num;
 
-            //Console.WriteLine("enteri array size: ");
-            //num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enteri array size: ");
+            num = Convert.ToInt32(Console.ReadLine());
 
-            //Console.WriteLine("write number");
-            //int[] arr1 = new int[num];
+            Console.WriteLine("write number");
+            int[] arr1 = new int[num];
 
-            //for (int i = 0; i < arr1.Length; i++)
-            //{
-            //    arr1[i] = Convert.ToInt32(Console.ReadLine());
-            //}
-            //var duplicates = arr1.GroupBy(a => a)
-            //    .Where(b => b.Count() > 1)
-            //    .Select(c => new { item = c.Key, count = c.Count(), sum = c.Sum() })
-            //    .ToList();
-            //Console.WriteLine(String.Join("\n", duplicates));
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                arr1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            var duplicates = arr1.GroupBy(a => a)
+                .Where(b => b.Count() > 1)
+                .Select(c => new { item = c.Key, count = c.Count(), sum = c.Sum() })
+                .ToList();
+            Console.WriteLine(String.Join("\n", duplicates));
 
             #endregion
 
